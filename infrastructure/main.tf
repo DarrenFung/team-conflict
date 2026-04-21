10acc9a885ca16aa37f714c08b837d47121ed016
Darren Fung <me@darrenfung.ca>
2026-04-21 13:32:43 -0400

    ✨ feat: add Cloud SQL Postgres instance and team_conflict database

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -21,3 +21,61 @@ resource "google_project_service" "serviceusage" {
   service            = "serviceusage.googleapis.com"
   disable_on_destroy = false
 }
+
+# =============================================================================
+# Cloud SQL instance
+# =============================================================================
+
+resource "google_sql_database_instance" "main" {
+  name             = var.instance_name
+  database_version = var.postgres_version
+  region           = var.region
+
+  deletion_protection = var.deletion_protection
+
+  depends_on = [google_project_service.sqladmin]
+
+  settings {
+    tier              = var.tier
+    availability_type = "ZONAL"
+    disk_size         = var.disk_size_gb
+    disk_type         = "PD_SSD"
+    disk_autoresize   = true
+
+    backup_configuration {
+      enabled                        = true
+      start_time                     = "03:00"
+      point_in_time_recovery_enabled = true
+
+      backup_retention_settings {
+        retained_backups = 7
+        retention_unit   = "COUNT"
+      }
+    }
+
+    ip_configuration {
+      ipv4_enabled = true
+      ssl_mode     = "ENCRYPTED_ONLY"
+
+      authorized_networks {
+        name  = "public"
+        value = "0.0.0.0/0"
+      }
+    }
+
+    database_flags {
+      name  = "cloudsql.iam_authentication"
+      value = "on"
+    }
+  }
+}
+
+# =============================================================================
+# Database
+# =============================================================================
+
+resource "google_sql_database" "main" {
+  name     = var.database_name
+  instance = google_sql_database_instance.main.name
+  charset  = "UTF8"
+}
