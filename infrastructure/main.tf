a30c54035ec49ffb273684ed055cc162cc7136cb
Darren Fung <me@darrenfung.ca>
2026-04-21 13:31:24 -0400

    🔧 chore: enable required GCP APIs via Terraform

---
+++ infrastructure/main.tf
@@ -0,0 +1,23 @@
+# =============================================================================
+# APIs
+# =============================================================================
+
+resource "google_project_service" "sqladmin" {
+  service            = "sqladmin.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "secretmanager" {
+  service            = "secretmanager.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "iam" {
+  service            = "iam.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "serviceusage" {
+  service            = "serviceusage.googleapis.com"
+  disable_on_destroy = false
+}
